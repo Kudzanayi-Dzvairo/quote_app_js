1d80e6283381a50ae22155ba0d7537bc8aa24603
Kudzanayi Dzvairo <kudzanayi@Kudzanayis-MacBook-Air.local>
2019-05-29 04:58:09 -0400

    working on randomizing quotes

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ function getData() {
     fetch(url)
     .then(res => res.json())
     .then(quotes => {
-        
+        console.log(quotes)
+        let number = Math.floor(Math.random() * quotes.length)
+
+        quote.innerHTML = '<span>"</span>' + quotes[number].quote+'<span>"</span>'
+        author.innerHTML = '<span>---</span>' + quotes[number].quote+'<span>"</span>'
+
+    })
+    .catch(error => {
+    //if there is an error
     })
 }
